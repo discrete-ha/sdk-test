0555dd719d676fbea27049479614a7d826f71cca
John <discreteb@gmail.com>
2018-12-26 17:33:47 +0900

    Update testSdk.js

--- testSdk.js
+++ testSdk.js
@@ -1,4 +1,6 @@
 var testSdk = function(e){
+	"use strict";
+	
 	document.addEventListener("DOMContentLoaded", function () {
 		var btns = document.getElementsByClassName('spacely-sdk-test-button');
 		for (var i = 0; i < btns.length; i++) {
